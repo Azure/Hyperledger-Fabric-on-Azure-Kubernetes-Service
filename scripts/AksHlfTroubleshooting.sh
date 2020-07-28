35b6476f1e8e388f1ab11c0d275c26c82b5a8b9e
Shruti Gupta <shrugupt@microsoft.com>
2020-07-28 13:37:50 +0530

    Updated troubleshooting script

--- scripts/AksHlfTroubleshooting.sh
+++ scripts/AksHlfTroubleshooting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function printErrorMessage() {
+function printUsage() {
     echo "Usage:"
     echo -e "./AksHlfTroubleshooting.sh <subscriptionID> <resourceGroup> <aksClusterName> <organizationType>"
     echo "Arguments:"
@@ -9,28 +9,84 @@ function printErrorMessage() {
     echo -e "\torganizationType  : Specify \"peer\" for peer organization and \"orderer\" for orderer organization"
 }
 
+function collectHlfNodesLogs() {
+    echo "Start collecting hlf nodes logs..."
+    {
+        echo "================= HLF NODES LOGS ================"
+        kubectl get all -n hlf
+    
+        for (( i=1; i<=$nodeCount; i++ ))
+        do
+            echo "------- Start: $organizationType$i node description -------------"
+            kubectl describe pod --selector="name=$organizationType$i" -n hlf
+
+            echo "------- End: $organizationType$i node description -------------"
+    
+            containers=($(kubectl get pods -l "name=$organizationType$i" -n hlf -o jsonpath={.items[*].spec.containers[*].name}))
+            podName=$(kubectl get pods -l "name=$organizationType$i" -ojsonpath={.items[0].metadata.name} -n hlf)
+            for container in "${containers[@]}"; do
+              echo "------- Start: $organizationType$i node $container container logs -------------"
+              kubectl logs $podName -n hlf -c $container
+              echo "------- End: $organizationType$i node $container container logs -------------"
+            done
+        done
+    } > $outputPath/nodes.output
+    echo "End collecting hlf nodes logs..."
+}
+
+function collectNginxLogs() {
+    {
+        echo "================= NGINX LOGS ================"
+        kubectl get all -n nginx
+        echo "------- Start: nginx controller logs -------------"
+        kubectl logs $(kubectl get pods -l "app=nginx-ingress" -ojsonpath={.items[0].metadata.name} -n nginx) -n nginx
+        echo "------- End: nginx controller logs -------------"
+    } > $outputPath/nginx.output
+}
+
+function collectIngressLogs() {
+    echo "Start Collecting ingress logs..."
+    {
+        echo "================= INGRESS LOGS ================"
+        echo "------- Start: ingress logs -------------"
+        kubectl get ingress --all-namespaces -o json
+        echo "------- End: ingress logs -------------"
+    } > $outputPath/ingress.output
+
+    echo "End Collecting ingress logs..."
+}
+
 if [ $# -ne 4 ]; then
     echo "Invalid arguments count!!"
-    printErrorMessage
+    printUsage
     exit 1
 fi
+
 subscriptionID=$1
 resourceGroup=$2
 aksClusterName=$3
 organizationType=$4
 
+which az
+if [ "$?" -ne 0 ]; then
+  echo "azure cli not found!!"
+  echo "azure cli is mandatory for this script."
+  echo "Please install azure cli and login with valid credentials"
+  exit 1
+fi
+
 az aks get-credentials -g $resourceGroup -n $aksClusterName --subscription $subscriptionID
 res=$?
 if [ $res -ne 0 ]; then
-    echo "Invalid arguments value!!"
-    printErrorMessage
+    echo "Fetching AKS cluster credential failed with error code $res!!"
+    printUsage
     exit 1
 fi
-echo "Connected to the cluster..."
+echo "Connected to the AKS cluster..."
 
 nodeCount=$(kubectl get configmap -n hlf-admin org-detail -o jsonpath={.data.nodeCount})
-orgName=$(kubectl get configmap -n hlf-admin org-detail -o jsonpath={.data.nodeCount})
-domainName=$(kubectl get configmap -n hlf-admin org-detail -o jsonpath={.data.nodeCount})
+orgName=$(kubectl get configmap -n hlf-admin org-detail -o jsonpath={.data.orgName})
+domainName=$(kubectl get configmap -n hlf-admin org-detail -o jsonpath={.data.domainName})
 outputPath="./output"
 zipFileName="AksHlfTroubleshooting-$(date "+%Y-%m-%d-%T").zip"
 rm -rf $outputPath
@@ -40,46 +96,10 @@ mkdir -p $outputPath
     echo "================= ORG METADATA ================"
     echo "Organization Name: $orgName, HLF node count: $nodeCount, Domain name: $domainName"
 } > $outputPath/metadata.output
-echo "Collected organization metadata..."
 
-{
-    echo "================= HLF NODES LOGS ================"
-    kubectl get all -n hlf
-
-    for (( i=1; i<=$nodeCount; i++ ))
-    do
-        echo "------- Start: $organizationType$i node description -------------"
-        kubectl describe pod --selector="name=peer$i" -n hlf
-        echo "------- End: $organizationType$i node description -------------"
-
-        echo "------- Start: $organizationType$i node logs -------------"
-        if [ $organizationType = "peer" ]; then
-            kubectl logs $(kubectl get pods -l "name=peer$i" -ojsonpath={.items[0].metadata.name} -n hlf) -n hlf -c peer$i
-            kubectl logs $(kubectl get pods -l "name=peer$i" -ojsonpath={.items[0].metadata.name} -n hlf) -n hlf -c couchdb$i
-        else
-            kubectl logs $(kubectl get pods -l "name=orderer$i" -ojsonpath={.items[0].metadata.name} -n hlf) -n hlf
-        fi
-        echo "------- End: $organizationType$i node logs -------------"
-    done
-} > $outputPath/nodes.output
-echo "Collected hlf nodes logs..."
-
-{
-    echo "================= NGINX LOGS ================"
-    kubectl get all -n nginx
-    echo "------- Start: nginx controller logs -------------"
-    kubectl logs $(kubectl get pods -l "app=nginx-ingress" -ojsonpath={.items[0].metadata.name} -n nginx) -n nginx
-    echo "------- End: nginx controller logs -------------"
-} > $outputPath/nginx.output
-echo "Collected nginx logs..."
-
-{
-    echo "================= INGRESS LOGS ================"
-    echo "------- Start: ingress logs -------------"
-    kubectl get ingress --all-namespaces -o json
-    echo "------- End: ingress logs -------------"
-} > $outputPath/ingress.output
-echo "Collected ingress logs..."
+collectHlfNodesLogs
+collectNginxLogs
+collectIngressLogs
 
 zip -r $zipFileName output/
 echo "Logs are stored at path: $(pwd)/$zipFileName"
