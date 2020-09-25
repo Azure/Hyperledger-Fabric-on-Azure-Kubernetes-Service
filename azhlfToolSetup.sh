631e823f8237ad11e32b6655542efcaa1fdd12c6
Chintan Rajvir <chintanr97@gmail.com>
2020-09-25 14:57:54 +0530

    Get the latest azhlfTool from the new branch

--- azhlfToolSetup.sh
+++ azhlfToolSetup.sh
@@ -7,8 +7,8 @@ git init
 git config core.sparsecheckout true
 echo "azhlfTool" > .git/info/sparse-checkout
 git remote add -f origin https://github.com/Azure/Hyperledger-Fabric-on-Azure-Kubernetes-Service.git
-git reset --hard origin/master
-git pull origin master
+git reset --hard origin/azhlfTool1.4.4-absca
+git pull origin azhlfTool1.4.4-absca
 echo
 echo "===> Done"
 echo
